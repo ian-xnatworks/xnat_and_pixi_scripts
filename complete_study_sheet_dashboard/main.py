427c19bbb2ca8b36a19d22a62eb2026fa623aa55
ian-xnatworks <ian@xnatworks.io>
2026-01-23 17:32:07 -0500

    Setting up basic UI.

--- complete_study_sheet_dashboard/main.py
+++ complete_study_sheet_dashboard/main.py
@@ -9,6 +9,13 @@ import csv
 import argparse
 import json
 
+css='''
+<style>
+    section.main > div {max-width: 80%;}
+</style>
+'''
+st.markdown(css, unsafe_allow_html=True)
+
 class App:
 
     def __init__(self, host=None, user=None, password=None, project_id=None):
@@ -25,6 +32,39 @@ class App:
                 raise Exception(f'Error connecting to project {self._project_id}', e)
         else:
             raise Exception('Must be started from an XNAT project.')
+
+        self._init_session_state()
+        self.init_ui()
+
+    def init_session_state(self):
+        # Initialize streamlit session state
+        # Values will be populated later
+        if 'project' not in st.session_state:
+            st.session_state.project = self._project
+
+        if 'project_id' not in st.session_state:
+            st.session_state.project_id = self._project_id
+
+        if 'experiments' not in st.session_state:
+            st.session_state.experiments = []
+
+    def init_ui(self):
+        # Hide streamlit deploy button
+        st.markdown("""
+            <style>
+                .reportview-container {
+                    margin-top: -2em;
+                }
+                #MainMenu {visibility: hidden;}
+                .stDeployButton {display:none;}
+                footer {visibility: hidden;}
+                #stDecoration {display:none;}
+            </style>
+        """, unsafe_allow_html=True)
+
+        # Initialize UI
+        self._init_sidebar()
+
 
     def init_options_sidebar(self):
         # Streamlit setup
